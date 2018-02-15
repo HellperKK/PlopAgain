a6346f7d8f8bf8464851f91547d9dfc5b5e83061
François Berhn <baudetad@gmail.com>
2018-02-15 09:36:51 +0100

    update

--- Main.php
+++ Main.php
@@ -2,6 +2,7 @@
 //Appel de la librarie
 	include "Src/Base.php";
 //Creation de la classe de la page
-	echo "Hello !"
+//~ 	echo "Hello !";
 //Creation de l'instance de page
+	echo Element::make_title1("Hello world !", ["id"=>"test"])->render();
 ?>
--- Src/Base.php
+++ Src/Base.php
@@ -1,22 +1,40 @@
-<?php 	
-
-	class MonoElement {
-		function __construct() {
-			$this->id = "";
-			$this->classe = "";
+<?php 
+	class PlainText{
+		function __construct($content=""){
+			$this->content = $content;
+		}
+		function render(){
+			return $this->content;
 		}
 	}
-	
-	class SimpleElement extends MonoElement{ 
-		function __construct($test) {
-			$this->value = $test;
-			$this->id = "";
-			$this->classe = "";
+	class Element {
+		public static function make_paragraph($text, $attr=[]){
+			$a = new PlainText($text);
+			return new Element("p", true, [$a], $attr);
 		}
-	}
-	class ComplexElement{
-		function __construct(){
-			$this->value = [];
+		public static function make_title1($text, $attr=[]){
+			$a = new PlainText($text);
+			return new Element("h1", true, [$a], $attr);
+		}
+		function __construct($name, $closed=true, $content=[], $attr=[]){
+			$this->content = $content;
+			$this->attr = $attr;
+			$this->closed = $closed;
+			$this->name = $name;
+		}
+		function render(){
+			$result = "<" . $this->name;
+			foreach($this->attr as $index => $item){
+				$result = $result . " " . $index . "=" . $item;
+			}
+			$result = $result . ">";
+			foreach($this->content as $item){
+				$result = $result . "\n" . $item->render();
+			}
+			if($this->closed){
+				$result = $result . "</" . $this->name . ">";
+			}
+			return $result;
 		}
 	}
 ?>
