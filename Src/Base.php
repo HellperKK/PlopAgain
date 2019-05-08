bacf3c351ab2d0174e40a6f2e032c1d8766336a2
Adrien Baudet <baudetad@gmail.com>
2019-05-08 10:57:07 +0200

    renommage et ajout br et set_attr

--- Src/Base.php
+++ Src/Base.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	class PlainText{
 		function __construct($content=""){
 			$this->content = $content;
@@ -8,35 +8,43 @@
 		}
 	}
 	class Element {
-		public static function make_paragraph($text, $attr=[]){
+		public static function paragraph($text, $attr=[]){
 			$a = new PlainText($text);
 			return new Element("p", true, [$a], $attr);
 		}
-		public static function make_title1($text, $attr=[]){
+		public static function title1($text, $attr=[]){
 			$a = new PlainText($text);
 			return new Element("h1", true, [$a], $attr);
 		}
-		public static function make_title2($text, $attr=[]){
+		public static function title2($text, $attr=[]){
 			$a = new PlainText($text);
 			return new Element("h2", true, [$a], $attr);
 		}
-		public static function make_title3($text, $attr=[]){
+		public static function title3($text, $attr=[]){
 			$a = new PlainText($text);
 			return new Element("h3", true, [$a], $attr);
 		}
-		public static function make_title4($text, $attr=[]){
+		public static function title4($text, $attr=[]){
 			$a = new PlainText($text);
 			return new Element("h4", true, [$a], $attr);
 		}
-		public static function make_title5($text, $attr=[]){
+		public static function title5($text, $attr=[]){
 			$a = new PlainText($text);
 			return new Element("h5", true, [$a], $attr);
 		}
-		
-		public static function make_div($content=[], $attr=[]){
+
+		public static function div($content=[], $attr=[]){
 			return new Element("div", true, $content, $attr);
 		}
-		
+
+		public static function custom($name, $content=[], $attr=[]){
+			return new Element($name, true, $content, $attr);
+		}
+
+		public static function br($content=[], $attr=[]){
+			return new Element("br", false, $content, $attr);
+		}
+
 		function __construct($name, $closed=true, $content=[], $attr=[]){
 			$this->content = $content;
 			$this->attr = $attr;
@@ -46,16 +54,20 @@
 		function render(){
 			$result = "<" . $this->name;
 			foreach($this->attr as $index => $item){
-				$result = $result . " " . $index . "=" . $item;
+				$result = $result . " " . $index . "='" . $item . "'";
 			}
 			$result = $result . ">";
-			foreach($this->content as $item){
-				$result = $result . "\n" . $item->render();
-			}
 			if($this->closed){
+				foreach($this->content as $item){
+					$result = $result . "\n" . $item->render();
+				}
 				$result = $result . "</" . $this->name . ">";
 			}
 			return $result;
+		}
+
+		function set_attr($name, $value){
+			$this->attr[$name] = $value;
 		}
 	}
 ?>
